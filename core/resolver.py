966be98f8ec57f2968934cf7fa41e7774ff709dd
Subhan Adeel <ms1640070@gmail.com>
2026-06-30 17:51:59 +0500

    updated the client key improvement

--- core/resolver.py
+++ core/resolver.py
@@ -9,4 +9,19 @@ class KeyResolver:
         if self.func:
             return self.func(*args, **kwargs)
 
+
+        # auto detect request object
+        for arg in args:
+
+            if hasattr(arg, "client"):
+                return arg.client.host
+
+
+        for value in kwargs.values():
+
+            if hasattr(value, "client"):
+                return value.client.host
+
+
+
         return "anonymous"
